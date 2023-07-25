9116eea6f19f227e5cc2a624fa4ae3c3d5f90b35
Atikur Rahaman <atikurahad@gmail.com>
2023-07-26 00:48:22 +0600

    practice

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-document.write("hello world");
+// document.write("hello world");
 
-let result = 99;
-if (result<80 && result>=70) {
-   document.write("A");
-}
-if (result<90 && result>=80) {
-   document.write("A+");
-}
-if (result>90 && result<=100) {
-   document.write("Golden A+");
-}
+// let result = 99;
+// if (result<80 && result>=70) {
+//    document.write("A");
+// }
+// if (result<90 && result>=80) {
+//    document.write("A+");
+// }
+// if (result>90 && result<=100) {
+//    document.write("Golden A+");
+// }
+
+
+const myArr = [23,45,56,90,43,55,55,9];
+ const myName ={
+   name:"Atikur",
+   age : 23,
+   city:"Dhaka",
+   country:"Bangladesh"
+
+ }
+ for (let i  in myName){
+   document.write(`${i[0]} <br/>`);
+ }
