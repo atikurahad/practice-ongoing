a83706bfb51dce21cc414e22f8fa2cb558a5977d
Atikur Rahaman <atikurahad@gmail.com>
2023-07-25 13:37:25 +0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 document.write("hello world");
 
-prompt()
+let result = 99;
+if (result<80 && result>=70) {
+   document.write("A");
+}
+if (result<90 && result>=80) {
+   document.write("A+");
+}
+if (result>90 && result<=100) {
+   document.write("Golden A+");
+}
